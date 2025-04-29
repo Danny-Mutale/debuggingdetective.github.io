463de09d4f175a60b9ffde27b8bae9a03fcbc683
Danny-Mutale <40736617@live.napier.ac.uk>
2025-04-29 12:31:02 +0100

    Update index.js

--- JAVASCRIPT/index.js
+++ JAVASCRIPT/index.js
@@ -1,7 +1,9 @@
+//loads the pregame page
 document.getElementById('start-game').addEventListener('click', () => {
     window.location.href = 'pregame.html';
 });
 
+//loads the instructions page
 document.getElementById('instructions').addEventListener('click', () => {
     window.location.href = 'instructions.html';
 });
