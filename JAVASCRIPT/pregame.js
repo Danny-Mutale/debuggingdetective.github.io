15e60f392f024cac4bc8f8705a1305d8df02efed
Danny-Mutale <40736617@live.napier.ac.uk>
2025-04-29 11:52:37 +0100

    Update pregame.js

--- JAVASCRIPT/pregame.js
+++ JAVASCRIPT/pregame.js
@@ -1,3 +1,5 @@
+//This function picks up when a language is selected and directs to the game page
+
 document.getElementById('languageForm').addEventListener('submit', (e) => {
     e.preventDefault();
     const language = document.querySelector('input[name="language"]:checked').value;
